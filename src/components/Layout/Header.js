6608ddb8adb192a06e94f1a13465565a952fc89f
theToxicKnight <michaelomordia.mo@gmail.com>
2021-05-11 19:50:19 -0500

    Cart and modal state to exit out of cart cleanly

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,7 +9,7 @@ const Header = (props) => {
     <React.Fragment>
       <header className={classes.header}>
         <h1>Wong Fei Hung Chinese quisines</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={Image} />
